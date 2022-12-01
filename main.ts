0aecb1df49e4cdaa0ff49ec18ca82c936202634e
albondiga0712 <119592963+albondiga0712@users.noreply.github.com>
2022-12-01 12:31:15 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendString("hfdjks")
+    radio.sendString(":)")
 })
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendString("Bfjdsk")
+    radio.sendString(":(")
 })
 radio.setGroup(1)
 basic.forever(function () {
